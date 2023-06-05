4182f62fe207a7da90ee23794a92290ad2c15509
Nezzar-Marwa <marwa.nezzar@edu.univ-fcomte.fr>
2023-06-05 14:16:32 +0200

    page d'accueil

--- S2_MN_KK_MCN_CM/src/pages/index.vue
+++ S2_MN_KK_MCN_CM/src/pages/index.vue
@@ -1,3 +1,67 @@
 <template>
-  <h1>Page d'accueil</h1>
+  <header>
+    <img src="/img/homepage_mobile.webp" alt="image présentation foxtreme" class="lg:hidden w-full">
+    <img src="/img/homepage_desktop.webp" alt="image homepage 2" class="hidden lg:block w-full h-[600px]">
+    <h1 class="h2 font-bold">Donnez le meilleur de vous même avec Foxtreme.</h1>
+
+    <p>La ville et sa pollution vous rendent anxieux, fatigués et déprimés ? 
+  Vous aimez les défis seul ou en équipe et surpassez vos limite ?</p>
+
+  <div>
+  <h1> Foksers est LA solution qu'il vous faut ! </h1>
+  <div>
+    <img src="/img/hp_1.webp" alt="illustration 1">
+    <p>Fokser est un site permettant de regrouper des informations générales sur plusieurs climats et écosystème, il a un but informatif et applicatif permettant d’obtenir des informations, de partage et d’échanges autour d’activités en pleine nature tel que la randonnée, le camping, la pêche et autres.
+</p>
+  </div>
+  <div>
+    <img src="/img/hp_2.webp" alt="illustration">
+    <p>Foksers vous donne accès</p>
+<li>à des cartes intéractives</li>
+<li>une communauté de passionnés</li>
+<li>un espace d’échange</li>
+<li>des informations inédites sur la faune et la flaure</li>
+  </div>
+  <div class="flex justify-center items-center mb-7 ">
+     <button
+       class=" rounded-lg border-2 border-text px-5 py-2 font-semibold text-text hover:bg-text hover:text-backgroundcolor">
+       <RouterLink to="/connexion">s'inscrire</RouterLink>
+     </button>
+    </div>
+  </div>
+
+<h2>Nous avons pour devise de vous aider à réaliser vos projets les plus ambitieux et de vous mettre en relation avec des gens ayant un goût prononcé pour la survie et les promenades en nature comme vous</h2>
+
+<section>
+    <div class=" bg-contrastcolor rounded-3xl flex lg:mx-96 mb-9">
+      <img src="/img/hp_3.webp" alt="card 1" class="w-[150px]">
+      <div class="">
+      <h1 class="h5 text-backgroundcolor font-medium pt-7 mb-10 mx-8">Envie de découvrir le monde de la survie ?</h1>
+      <p class="h4 text-backgroundcolor mx-8">Accedez a nos articles qui vous donnerons les bases pour bien débuter vos aventures et qui vous permetteront de rester informer sur les nouveauté dans le monde de la survie.</p>
+      </div>
+    </div>
+
+        <div class=" bg-contrastcolor rounded-3xl flex lg:mx-96 mb-9">
+        <img src="/img/hp_4.webp" alt="card 1" class="w-[150px]">
+        <div>
+        <h1 class="h5 text-backgroundcolor font-medium pt-7 mb-10 mx-8">Besoin d’un conseil ?</h1>
+        <p class="h4 text-backgroundcolor mx-8">Demandez à la communauté de foxtrème qui saura vous aiguiller et vous  soutenir dans vos projets. Vous pourrez échanger et partager de bons souvenirs.</p>
+        </div>
+      </div>
+
+        <div class=" bg-contrastcolor rounded-3xl flex lg:mx-96">
+          <img src="/img/hp_5.webp" alt="card 1" class="w-[150px]">
+          <div>
+          <h1 class="h5 text-backgroundcolor font-medium pt-7 mb-10 mx-8">Envie de changement</h1>
+          <p class="h4 text-backgroundcolor mx-8">Vous voulez découvrir de nouveaux lieux mais ne savez pas par où commencer ? N’attendez plus Foxtrème vous permet d’accéder à des lieux merveilleux dans toute la France et proche de chez vous.</p>
+          </div>
+        </div>
+</section>
+
+<section>
+  <h1 class="h3 font-semibold text-center mt-7 mb-2">Partez à l’aventure dans toute la France dès maintenant</h1>
+  <h2 class="h4 text-center">Nos coups de coeur du moment</h2>
+</section>
+
+  </header>
 </template>
