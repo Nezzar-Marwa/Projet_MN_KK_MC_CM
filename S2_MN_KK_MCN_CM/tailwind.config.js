21d7de22183cf248adf154a148b6c541eb366ae7
Nezzar-Marwa <marwa.nezzar@edu.univ-fcomte.fr>
2023-06-05 09:14:12 +0200

    installation header

---
+++ S2_MN_KK_MCN_CM/tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+
+    colors: {
+      toniccolor: '#ff9c40',
+      backgroundcolor: '#f5f5f5',
+      contrastcolor: '#2c2b2b',
+      contentbluelayout: '#2f8a99',
+      green: '#2c2c2c',
+      text: '#000000',
+      contentbluecards: '#8ecdd7',
+      teamcolor: '#434343',
+      invalid: '#f42e2e'
+    },
+
+    fontFamily: {
+      jost: ["Jost"],
+      poppins: ["Poppins"]
+    },
+    extend: {
+      gridTemplateColumns: {
+        principal: 'repeat(12, minmax(0, 64px))'
+      },
+    },
+
+    plugins: [],
+  }
+}
+
+
