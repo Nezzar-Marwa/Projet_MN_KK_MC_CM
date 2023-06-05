a629d8fcf0c2e77fecec3dc1357db2323c7ba2e0
Nezzar-Marwa <marwa.nezzar@edu.univ-fcomte.fr>
2023-06-05 15:49:24 +0200

    homepage

--- S2_MN_KK_MCN_CM/src/pages/index.vue
+++ S2_MN_KK_MCN_CM/src/pages/index.vue
@@ -5,18 +5,20 @@
     <h1 class="h2 font-bold mb-4 mx-4">Donnez le meilleur de vous même avec Foxtreme.</h1>
 
     <p class="h3 mx-4">La ville et sa pollution vous rendent anxieux, fatigués et déprimés ? </p>
-    <p class="h3 mb-11 mx-4"> Vous aimez les défis seul ou en équipe et surpassez vos limite ?</p>
+    <p class="h3 mb-11 lg:mb-28 mx-4"> Vous aimez les défis seul ou en équipe et surpassez vos limite ?</p>
 
   <div>
   <h1 class="h2 font-bold text-center mb-8"> Foksers est LA solution qu'il vous faut ! </h1>
-  <div>
-    <img src="/img/hp_1.webp" alt="illustration 1" class="mb-5 m-auto w-[500px]">
-    <p class="bg-contrastcolor rounded-2xl text-backgroundcolor mb-8 py-4 lg:mx-24 px-4 lg:px-14">Fokser est un site permettant de regrouper des informations générales sur plusieurs climats et écosystème, il a un but informatif et applicatif permettant d’obtenir des informations, de partage et d’échanges autour d’activités en pleine nature tel que la randonnée, le camping, la pêche et autres.
-</p>
+  
+  <div class="grille lg:mb-16">
+  <div class="col-span-5 col-start-2 bg-contrastcolor rounded-2xl text-backgroundcolor mb-8 lg:py-20 py-8 px-4 lg:px-14"><p>Fokser est un site permettant de regrouper des informations générales sur plusieurs climats et écosystème, il a un but informatif et applicatif permettant d’obtenir des informations, de partage et d’échanges autour d’activités en pleine nature tel que la randonnée, le camping, la pêche et autres.
+  </p></div>
+    <img src="/img/hp_1.webp" alt="illustration 1" class="mb-5 m-auto w-[500px] col-span-5 col-start-7">
   </div>
-  <div>
-    <img src="/img/hp_2.webp" alt="illustration" class="mb-5 m-auto w-[500px]">
-    <div class="bg-contrastcolor rounded-2xl text-backgroundcolor mb-8 py-4 lg:mx-24 px-14">
+  
+  <div class="grille lg:mb-16">
+    <img src="/img/hp_2.webp" alt="illustration" class="mb-5 m-auto w-[500px] col-span-5 col-start-2">
+    <div class="col-span-5 col-start-7 bg-contrastcolor rounded-2xl text-backgroundcolor mb-8 lg:py-28 py-8 px-14 lg:pl-20">
 <p>Foksers vous donne accès</p>
 <li>à des cartes intéractives</li>
 <li>une communauté de passionnés</li>
@@ -26,7 +28,7 @@
   </div>
   <div class="flex justify-center items-center mb-7 ">
      <button
-       class=" rounded-lg border-2 border-text px-5 py-2 font-semibold text-text hover:bg-text hover:text-backgroundcolor">
+       class="lg:mb-16 rounded-lg border-2 border-text px-5 py-2 font-semibold text-text hover:bg-text hover:text-backgroundcolor">
        <RouterLink to="/connexion">s'inscrire</RouterLink>
      </button>
     </div>
@@ -35,32 +37,34 @@
 <h2 class="text-center h3 mx-10 mb-8">Nous avons pour devise de vous aider à réaliser vos projets les plus ambitieux et de vous mettre en relation avec des gens ayant un goût prononcé pour la survie et les promenades en nature comme vous</h2>
 
 <section>
-    <div class=" bg-contrastcolor rounded-3xl flex lg:mx-96 mb-9">
-      <img src="/img/hp_3.webp" alt="card 1" class="w-[130px]">
+
+    <RouterLink to="/ressources" class=" bg-contrastcolor rounded-3xl flex lg:mx-[450px] mb-9">
+      <img src="/img/hp_3.webp" alt="card 1" class="w-[130px] lg:w-[140px]">
       <div class="">
-      <h1 class="h5 text-backgroundcolor font-medium pt-4 lg:pt-7 mb-4 lg:mb-10 mx-2">Envie de découvrir le monde de la survie ?</h1>
+      <h1 class="h5 text-backgroundcolor font-medium pt-4 lg:pt-7 mb-4 lg:mb-4 mx-8">Envie de découvrir le monde de la survie ?</h1>
       <p class=" lg:hidden h4 text-backgroundcolor lg:mx-8 mx-2">Accedez a nos articles qui vous donnerons les bases pour bien débuter vos aventures.</p>
-      <p class="hidden lg:block h4 text-backgroundcolor lg:mx-8">Accedez a nos articles qui vous donnerons les bases pour bien débuter vos aventures et qui vous permetteront de rester informer sur les nouveauté dans le monde de la survie.</p>
+      <p class="hidden lg:block h4 text-backgroundcolor lg:mx-8 lg:pb-8">Accedez a nos articles qui vous donnerons les bases pour bien débuter vos aventures et qui vous permetteront de rester informer sur les nouveauté dans le monde de la survie.</p>
       </div>
-    </div>
+    </RouterLink>
+ 
 
-        <div class=" bg-contrastcolor rounded-3xl flex lg:mx-96 mb-9">
-        <img src="/img/hp_4.webp" alt="card 1" class="w-[130px]">
+        <RouterLink to="/communaute" class=" bg-contrastcolor rounded-3xl flex lg:mx-[450px] mb-9">
+        <img src="/img/hp_4.webp" alt="card 1" class="w-[130px] lg:w-[140px]">
         <div>
-        <h1 class="h5 text-backgroundcolor font-medium pt-4 lg:pt-7 mb-4 lg:mb-10 mx-2">Besoin d’un conseil ?</h1>
+        <h1 class="h5 text-backgroundcolor font-medium pt-4 lg:pt-7 mb-4 lg:mb-4 mx-8">Besoin d’un conseil ?</h1>
         <p class="lg:hidden h4 text-backgroundcolor lg:mx-8 mx-2">Demandez à la communauté de foxtrème qui saura vous aiguiller et vous  soutenir dans vos projets.</p>
-        <p class="hidden lg:block h4 text-backgroundcolor lg:mx-8">Demandez à la communauté de foxtrème qui saura vous aiguiller et vous  soutenir dans vos projets. Vous pourrez échanger et partager de bons souvenirs.</p>
+        <p class="hidden lg:block h4 text-backgroundcolor lg:mx-8 lg:pb-8">Demandez à la communauté de foxtrème qui saura vous aiguiller et vous  soutenir dans vos projets. Vous pourrez échanger et partager de bons souvenirs.</p>
         </div>
-      </div>
+      </RouterLink>
 
-        <div class=" bg-contrastcolor rounded-3xl flex lg:mx-96">
-          <img src="/img/hp_5.webp" alt="card 1" class="w-[130px]">
+        <RouterLink to="/lieux" class=" bg-contrastcolor rounded-3xl flex lg:mx-[450px]">
+          <img src="/img/hp_5.webp" alt="card 1" class="w-[130px] lg:w-[140px]">
           <div>
-          <h1 class="h5 text-backgroundcolor font-medium pt-4 lg:pt-7 mb-4 lg:mb-10 mx-2">Envie de changement</h1>
+          <h1 class="h5 text-backgroundcolor font-medium pt-4 lg:pt-7 mb-4 lg:mb-4 mx-8">Envie de changement</h1>
           <p class="lg:hidden h4 text-backgroundcolor lg:mx-8 mx-2">Vous voulez découvrir de nouveaux lieux? N’attendez plus Foxtrème vous permet d’accéder à des lieux merveilleux.</p>
-          <p class="hidden lg:block h4 text-backgroundcolor lg:mx-8">Vous voulez découvrir de nouveaux lieux mais ne savez pas par où commencer ? N’attendez plus Foxtrème vous permet d’accéder à des lieux merveilleux dans toute la France et proche de chez vous.</p>
+          <p class="hidden lg:block h4 text-backgroundcolor lg:mx-8 lg:pb-8">Vous voulez découvrir de nouveaux lieux mais ne savez pas par où commencer ? N’attendez plus Foxtrème vous permet d’accéder à des lieux merveilleux dans toute la France et proche de chez vous.</p>
           </div>
-        </div>
+        </RouterLink>
 </section>
 
 <section>
