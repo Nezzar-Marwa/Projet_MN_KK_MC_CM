200544e3f855d03d5307b0f0d63d6c27088061e2
Nezzar-Marwa <marwa.nezzar@edu.univ-fcomte.fr>
2023-06-06 16:56:01 +0200

    ajout icon

--- S2_MN_KK_MCN_CM/src/pages/communaute.vue
+++ S2_MN_KK_MCN_CM/src/pages/communaute.vue
@@ -9,9 +9,23 @@ const articlesfav: ArticlesResponse[] = await articlesfavoris();
 <div class="pt-20 mb-28">
    <h1 class="h1 font-semibold uppercase text-center mb-6">Partagez</h1>
    <p class="h3 text-center">Envie de papoter ? N’hésitez plus !</p>
+
+   <section>
+      <h1>Sujets à la une</h1>
+
+      <div class="bg-backgroundcolor shadow-xl">
+         <div>
+            <img src="/img/profile1.webp">
+            <h1>tibidou758</h1>
+         </div>
+         <div>
+            <p>Bonjour à tous, Pourriez-vous me donner des conseils sur le matériel à prendre pour faire le sentier du rocher de Saint Julien sur plusieurs jours</p>
+         </div>
+      </div>
+   </section>
   </div>
 
-  
+
    <div class="lg:grille pt-6 flex overflow-hidden overflow-x-scroll gap-5  ">
        <ArticleCard v-for=" unarticle of articlesfav" :v-key="articlesfavoris" v-bind="{ ...unarticle }"  class="flex-none lg:col-span-4 " />
          </div>
