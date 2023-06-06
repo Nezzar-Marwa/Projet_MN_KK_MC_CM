595d9e48f6293b0121115c580fd018c83c4fd0aa
Nezzar-Marwa <marwa.nezzar@edu.univ-fcomte.fr>
2023-06-06 10:59:32 +0200

    ajout lieux map

--- S2_MN_KK_MCN_CM/src/components/GoogleMaps.vue
+++ S2_MN_KK_MCN_CM/src/components/GoogleMaps.vue
@@ -7,6 +7,10 @@
       class="mt-16 lg:mx-40">
       <Marker :options="{ position: center }" />
       <Marker :options="{ position: lieux1}"/>
+      <Marker :options="{ position: lieux2 }"/>
+      <Marker :options="{ position: lieux3 }"/>
+      <Marker :options="{ position: lieux4 }"/>
+      <Marker :options="{ position: lieux5 }"/>
       </GoogleMap>
 
       <div>
@@ -19,6 +23,12 @@
  
      const center = { lat: 47.49566525144965, lng: 6.805222081286718 }
      const lieux1 = { lat: 47.18559646606445, lng: 6.872682094573975 }
+     const lieux2 = { lat: 47.785, lng: 6.96693 }
+     const lieux3 = { lat: 45.04366, lng: 3.88523 }
+     const lieux4 = { lat: 44.86246, lng: 5.66926 }
+     const lieux5 = { lat: 44.25983, lng: 5.27339 }
+     const lieux6 = { lat: 47.18559646606445, lng: 6.872682094573975 }
+     const lieux7 = { lat: 47.18559646606445, lng: 6.872682094573975 }
      </script>
 
 
