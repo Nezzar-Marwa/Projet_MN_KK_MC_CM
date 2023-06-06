f0062d3ce02013df2733fe38444d7079d9ceefa4
Nezzar-Marwa <marwa.nezzar@edu.univ-fcomte.fr>
2023-06-06 10:23:03 +0200

    ajout lieux

--- S2_MN_KK_MCN_CM/src/components/GoogleMaps.vue
+++ S2_MN_KK_MCN_CM/src/components/GoogleMaps.vue
@@ -10,15 +10,19 @@
       :zoom="15"
       >
       <Marker :options="{ position: center }" />
+      <Marker :options="{ position: lieux1}"/>
       </GoogleMap>
 
-      
+      <div>
+      </div>
+
      </template>
  
      <script setup lang="ts">
      import { GoogleMap, Marker } from 'vue3-google-map'
  
      const center = { lat: 47.49566525144965, lng: 6.805222081286718 }
+     const lieux1 = { lat: 47.18559646606445, lng: 6.872682094573975 }
      </script>
 
 
--- S2_MN_KK_MCN_CM/src/pages/cartes.vue
+++ S2_MN_KK_MCN_CM/src/pages/cartes.vue
@@ -4,4 +4,27 @@ import GoogleMaps from '@/components/GoogleMaps.vue';
 <template>
     <h1>cartes</h1>
     <GoogleMaps />
+  <li>
+     <RouterLink to="/lieux/w3xvejv59lvpcg5">Le chemin de Régordane</RouterLink>
+     </li>
+<li>
+     <RouterLink to="/lieux/w1aiqdk84suzx5l">Passerelles himalayennes du Monteynard</RouterLink>
+ </li>
+
+  <li>
+         <RouterLink to="/lieux/1ubbz2160joyz7z">Autour du Rocher Saint-Julien</RouterLink>
+     </li>
+
+  <li>
+         <RouterLink to="/lieux/vefum1zne0glll2">Le ballon d'Alsace</RouterLink>
+     </li>
+      <li>
+         <RouterLink to="/lieux/4o1ugjtk1f8jh56">Le saut de la bourique</RouterLink>
+     </li>
+      <li>
+         <RouterLink to="/lieux/nnqzwdua12rp23h">La source Bleu</RouterLink>
+     </li>
+      <li>
+         <RouterLink to="/lieux/6y79gskwczatekk">Les échelles de la mort</RouterLink>
+     </li>
 </template>
